6aa4687122de6c2cd4975f71948e7c20cedf8cd5
Marin Aeschbach <marin.aeschbach@gmail.ch>
2020-06-14 22:38:01 +0200

    Extend theme

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 import { ThemeProvider } from 'emotion-theming'
+
+const space = [
+  0,
+  '0.25rem',
+  '0.5rem',
+  '0.75rem',
+  '1rem',
+  '1.5rem',
+  '2rem',
+  '3rem',
+  '4.5rem',
+  '6rem',
+]
 
 const theme = {
   colors: {
@@ -13,7 +25,30 @@ const theme = {
     blue: '#007ce0',
     navy: '#004175',
   },
+  fontSizes: ['0.6rem', '0.8rem', '1rem'],
+  space: space,
+  lineHeights: space,
+  breakpoints: ['512px', '768px', '1024px', '1280px'],
+  shadows: {
+    default: '5px 10px #888888',
+  },
+  fonts: {
+    default: `apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+    headings: 'Georgia, serif',
+  },
+  radii: {
+    small: '4px',
+    default: '8px',
+    large: '12px',
+  },
+  sizes: {
+    touchable: '2.5rem',
+    full: '100%',
+  },
 }
+
+;(theme as any).lineHeights.default = space[5]
+;(theme as any).fontSizes.default = space[4]
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,9 +57,9 @@ ReactDOM.render(
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
