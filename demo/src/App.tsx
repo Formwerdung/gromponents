4814ae139d0c3dbccd555d88b5bfc26d29fe1f8a
Marin Aeschbach <marin.aeschbach@gmail.ch>
2020-06-14 22:38:12 +0200

    Add example elements to demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,32 +1,44 @@
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import logo from './logo.svg';
-import './App.css';
-import { DopeEditor, Box } from 'good-react-components'
+import { DopeEditor, Box, Text } from 'good-react-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <DopeEditor />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Box color="main" backgroundColor="white">
-        <p css={theme => ({ color: theme.main })}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        </Box>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box m={4}>
+      <DopeEditor />
+      <p css={(theme) => ({ color: theme.main })}>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <Box
+        backgroundColor="main"
+        color="white"
+        p={[4, 6]}
+        margin={4}
+        display="flex"
+        boxShadow="default"
+        flexDirection="column"
+        borderRadius={['small', 'default', 'large']}
+      >
+        <span>Hello Kitchen Sink Box</span>
+        <span>Hello my lovelies</span>
+      </Box>
+      <Box backgroundColor="main" size="touchable" />
+      <Text
+        as="h1"
+        mt={4}
+        textColor="navy"
+        textShadow="default"
+        lineHeight={4}
+        textAlign="right"
+      >
+        Hello World
+      </Text>
+      <Text as="h2" fontFamily="headings">
+        What a nice intermittend heading
+      </Text>
+    </Box>
+  )
 }
 
-export default App;
+export default App
